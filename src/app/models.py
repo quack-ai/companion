3d9a02b8a89129d3fc980e8cacdf188d63add62a
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2023-07-27 19:14:42 -0700

    docs: Updates license notice

--- src/app/models.py
+++ src/app/models.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2023, Quack AI.
 
-# This program is licensed under the Attribution-NonCommercial-ShareAlike 4.0 International.
-# See LICENSE or go to <https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode.txt> for full license details.
+# All rights reserved.
+# Copying and/or distributing is strictly prohibited without the express permission of its copyright owner.
 
 from datetime import datetime
 from enum import Enum
