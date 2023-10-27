67b854eb6090d2800d5942efbd1ee99990b3ac61
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2023-10-27 16:25:57 +0200

    test: Adds tests for login

---
+++ src/tests/conftest.py
@@ -0,0 +1,42 @@
+import asyncio
+from typing import Generator
+
+import pytest
+import pytest_asyncio
+from httpx import AsyncClient
+from sqlalchemy.orm import sessionmaker
+from sqlmodel import SQLModel
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+from app.core.config import settings
+from app.db import engine
+from app.main import app
+
+
+@pytest.fixture(scope="session")
+def event_loop(request) -> Generator:
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest_asyncio.fixture
+async def async_client():
+    async with AsyncClient(app=app, base_url=f"http://{settings.API_V1_STR}") as client:
+        yield client
+
+
+@pytest_asyncio.fixture(scope="function")
+async def async_session() -> AsyncSession:
+    session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+    async with session() as s:
+        async with engine.begin() as conn:
+            await conn.run_sync(SQLModel.metadata.create_all)
+
+        yield s
+
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.drop_all)
+
+    await engine.dispose()
---
+++ src/tests/endpoints/test_login.py
@@ -0,0 +1,69 @@
+from typing import Any, Dict, Union
+from urllib.parse import parse_qs, urlparse
+
+import pytest
+from httpx import AsyncClient
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+from app.models import User
+
+USER_TABLE = [
+    {"id": 1, "login": "first_login", "hashed_password": "hashed_first_pwd", "scope": "user"},
+    {"id": 2, "login": "second_login", "hashed_password": "hashed_second_pwd", "scope": "user"},
+]
+
+
+@pytest.mark.parametrize(
+    ("payload", "status_code", "status_detail"),
+    [
+        ({"username": "foo"}, 422, None),
+        ({"github_token": "foo"}, 401, None),
+    ],
+)
+@pytest.mark.asyncio()
+async def test_login_with_github_token(
+    async_client: AsyncClient,
+    async_session: AsyncSession,
+    payload: Dict[str, Any],
+    status_code: int,
+    status_detail: Union[str, None],
+):
+    for entry in USER_TABLE:
+        async_session.add(User(**entry))
+    await async_session.commit()
+
+    response = await async_client.post("/login/token", json=payload)
+    assert response.status_code == status_code
+    if isinstance(status_detail, str):
+        assert response.json()["detail"] == status_detail
+
+
+@pytest.mark.parametrize(
+    ("scope", "redirect_uri", "status_code"),
+    [
+        ("read:user%20user:email%20repo", "https://app.quack-ai.com", 307),
+    ],
+)
+@pytest.mark.asyncio()
+async def test_authorize_github(
+    async_client: AsyncClient,
+    async_session: AsyncSession,
+    scope: Any,
+    redirect_uri: Any,
+    status_code: int,
+):
+    for entry in USER_TABLE:
+        async_session.add(User(**entry))
+    await async_session.commit()
+
+    response = await async_client.get("/login/authorize", params={"scope": scope, "redirect_uri": redirect_uri})
+    assert response.status_code == status_code
+    for key, _, val in response.headers._list:
+        if key == "location":
+            u = urlparse(val)
+            assert u.scheme == "https"
+            assert u.netloc == "github.com/login/oauth/authorize"
+            q = parse_qs(u.query)
+            assert q.keys() == {"scope", "client_id", "redirect_uri"}
+            assert q["scope"][0] == scope
+            assert q["redirect_uri"][0] == redirect_uri
