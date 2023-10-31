b45b6a856891643a38087e3f0fdcc108d424e6d4
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2023-10-31 19:01:42 +0100

    feat: Add several analysis options

--- src/app/api/api_v1/endpoints/compute.py
+++ src/app/api/api_v1/endpoints/compute.py
@@ -5,29 +5,46 @@
 
 from typing import List
 
-from fastapi import APIRouter, Depends, Security, status
+from fastapi import APIRouter, Depends, Path, Security, status
 
 from app.api.dependencies import get_current_user, get_guideline_crud, get_repo_crud
 from app.crud import GuidelineCRUD, RepositoryCRUD
 from app.models import UserScope
 from app.schemas.compute import ComplianceResult, Snippet
-from app.services.openai import openai_client
+from app.services.openai import ExecutionMode, openai_client
 from app.services.telemetry import telemetry_client
 
 router = APIRouter()
 
 
-@router.post("/analyze", status_code=status.HTTP_200_OK)
-async def analyze_snippet(
+@router.post("/analyze/{repo_id}", status_code=status.HTTP_200_OK)
+async def check_code_against_repo_guidelines(
     payload: Snippet,
+    repo_id: int = Path(..., gt=0),
     repos: RepositoryCRUD = Depends(get_repo_crud),
     guidelines: GuidelineCRUD = Depends(get_guideline_crud),
     user=Security(get_current_user, scopes=[UserScope.ADMIN, UserScope.USER]),
 ) -> List[ComplianceResult]:
-    telemetry_client.capture(user.id, event="compute-analysis", properties={"repo_id": payload.repo_id})
+    telemetry_client.capture(user.id, event="compute-analyze", properties={"repo_id": repo_id})
     # Check repo
-    await repos.get(payload.repo_id, strict=True)
+    await repos.get(repo_id, strict=True)
     # Fetch guidelines
-    guideline_list = [elt for elt in await guidelines.fetch_all(("repo_id", payload.repo_id))]
+    guideline_list = [elt for elt in await guidelines.fetch_all(("repo_id", repo_id))]
     # Run analysis
-    return openai_client.analyze_snippet(payload.code, guideline_list)
+    return openai_client.analyze_multi(payload.code, guideline_list, mode=ExecutionMode.MULTI)
+
+
+@router.post("/check/{guideline_id}", status_code=status.HTTP_200_OK)
+async def check_code_against_guideline(
+    payload: Snippet,
+    guideline_id: int = Path(..., gt=0),
+    guidelines: GuidelineCRUD = Depends(get_guideline_crud),
+    user=Security(get_current_user, scopes=[UserScope.ADMIN, UserScope.USER]),
+) -> ComplianceResult:
+    # Check repo
+    guideline = await guidelines.get(guideline_id, strict=True)
+    telemetry_client.capture(
+        user.id, event="compute-check", properties={"repo_id": guideline.repo_id, "guideline_id": guideline_id}
+    )
+    # Run analysis
+    return openai_client.analyze_mono(payload.code, guideline)
--- src/app/services/openai.py
+++ src/app/services/openai.py
@@ -5,8 +5,10 @@
 
 import json
 import logging
+from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
-from typing import List
+from enum import Enum
+from typing import Any, Dict, List
 
 import requests
 from fastapi import HTTPException, status
@@ -30,7 +32,33 @@ logger = logging.getLogger("uvicorn.error")
 __all__ = ["openai_client"]
 
 
-RESPONSE_SCHEMA = ObjectSchema(
+class ExecutionMode(str, Enum):
+    SINGLE = "single-thread"
+    MULTI = "multi-thread"
+
+
+MONO_SCHEMA = ObjectSchema(
+    type="object",
+    properties={
+        "is_compliant": FieldSchema(
+            type="boolean", description="whether the guideline has been followed in the code snippet"
+        ),
+        "comment": FieldSchema(
+            type="string",
+            description="instruction to make the snippet compliant, addressed to the developer who wrote the code. Should be empty if the snippet is compliant",
+        ),
+        # "suggestion": FieldSchema(type="string", description="the modified code snippet that meets the guideline, with minimal modifications. Should be empty if the snippet is compliant"),
+    },
+    required=["is_compliant", "comment"],
+)
+
+MONO_PROMPT = (
+    "As a code compliance agent, you are going to receive requests from user with two elements: a code snippet, and a guideline. "
+    "You should answer with an analysis result. The comment should be an empty string if the code is compliant with the guideline."
+)
+
+
+MULTI_SCHEMA = ObjectSchema(
     type="object",
     properties={
         "result": ArraySchema(
@@ -54,6 +82,12 @@ RESPONSE_SCHEMA = ObjectSchema(
     required=["result"],
 )
 
+MULTI_PROMPT = (
+    "As a code compliance agent, you are going to receive requests from user with two elements: a code snippet, and a list of guidelines. "
+    "You should answer with a list of compliance results, one for each guideline, no more, no less (in the same order). "
+    "For a given compliance results, the comment should be an empty string if the code is compliant with the corresponding guideline."
+)
+
 
 class OpenAIClient:
     ENDPOINT: str = "https://api.openai.com/v1/chat/completions"
@@ -73,35 +107,75 @@ class OpenAIClient:
             f"Using OpenAI model: {self.model} (created at {datetime.fromtimestamp(model_card.json()['created']).isoformat()})"
         )
 
-    def analyze_snippet(self, code: str, guidelines: List[Guideline], timeout: int = 10) -> List[ComplianceResult]:
+    def analyze_multi(
+        self, code: str, guidelines: List[Guideline], timeout: int = 10, mode: ExecutionMode = ExecutionMode.SINGLE
+    ) -> List[ComplianceResult]:
         # Check args before sending a request
         if len(code) == 0 or len(guidelines) == 0 or any(len(guideline.details) == 0 for guideline in guidelines):
             raise HTTPException(
                 status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="No code or guideline provided for analysis."
             )
         # Ideas: check which programming language & whether it's correct code
+        if mode == ExecutionMode.SINGLE:
+            parsed_response = self._analyze(
+                MULTI_PROMPT,
+                {"code": code, "guidelines": [guideline.details for guideline in guidelines]},
+                MULTI_SCHEMA,
+                timeout,
+            )["result"]
+            if len(parsed_response) != len(guidelines):
+                raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Invalid model response")
+        elif mode == ExecutionMode.MULTI:
+            with ThreadPoolExecutor() as executor:
+                tasks = [
+                    executor.submit(
+                        self._analyze, MONO_PROMPT, {"code": code, "guideline": guideline.details}, MONO_SCHEMA, timeout
+                    )
+                    for guideline in guidelines
+                ]
+            # Collect results
+            parsed_response = [task.result() for task in tasks]
+        else:
+            raise ValueError("invalid value for argument `mode`")
+
+        # Return with pydantic validation
+        return [
+            # ComplianceResult(is_compliant=res["is_compliant"], comment="" if res["is_compliant"] else res["comment"])
+            ComplianceResult(guideline_id=guideline.id, **res)
+            for guideline, res in zip(guidelines, parsed_response)
+        ]
+
+    def analyze_mono(self, code: str, guideline: Guideline, timeout: int = 10) -> ComplianceResult:
+        # Check args before sending a request
+        if len(code) == 0 or len(guideline.details) == 0:
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="No code or guideline provided for analysis."
+            )
+        res = self._analyze(MONO_PROMPT, {"code": code, "guideline": guideline.details}, MONO_SCHEMA, timeout)
+        # Return with pydantic validation
+        return ComplianceResult(guideline_id=guideline.id, **res)
+
+    def _analyze(
+        self, prompt: str, payload: Dict[str, Any], schema: ObjectSchema, timeout: int = 10
+    ) -> ComplianceResult:
         # Prepare the request
-        payload = ChatCompletion(
+        _payload = ChatCompletion(
             model=self.model,
             messages=[
                 OpenAIMessage(
                     role=OpenAIChatRole.SYSTEM,
-                    content=(
-                        "As a code compliance agent, you are going to receive requests from user with two elements: a code snippet, and a list of guidelines. "
-                        "You should answer with a list of compliance results, one for each guideline (in the same order). "
-                        "For a given compliance results, the comment should be an empty string if the code is compliant with the corresponding guideline."
-                    ),
+                    content=prompt,
                 ),
                 OpenAIMessage(
                     role=OpenAIChatRole.USER,
-                    content=json.dumps({"code": code, "guidelines": [guideline.details for guideline in guidelines]}),
+                    content=json.dumps(payload),
                 ),
             ],
             functions=[
                 OpenAIFunction(
                     name="analyze_code",
-                    description="Check code against a set of guidelines",
-                    parameters=RESPONSE_SCHEMA,
+                    description="Analyze code",
+                    parameters=schema,
                 )
             ],
             function_call={"name": "analyze_code"},
@@ -109,21 +183,13 @@ class OpenAIClient:
             frequency_penalty=self.frequency_penalty,
         )
         # Send the request
-        response = requests.post(self.ENDPOINT, json=payload.dict(), headers=self.headers, timeout=10)
+        response = requests.post(self.ENDPOINT, json=_payload.dict(), headers=self.headers, timeout=timeout)
 
         # Check status
         if response.status_code != 200:
             raise HTTPException(status_code=response.status_code, detail=response.json()["error"]["message"])
-        # Ideas: check the returned code can run
-        parsed_response = json.loads(response.json()["choices"][0]["message"]["function_call"]["arguments"])["result"]
-        if len(parsed_response) != len(guidelines):
-            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Invalid model response")
-        # Return with pydantic validation
-        return [
-            # ComplianceResult(is_compliant=res["is_compliant"], comment="" if res["is_compliant"] else res["comment"])
-            ComplianceResult(guideline_id=guideline.id, **res)
-            for guideline, res in zip(guidelines, parsed_response)
-        ]
+
+        return json.loads(response.json()["choices"][0]["message"]["function_call"]["arguments"])
 
 
 openai_client = OpenAIClient(settings.OPENAI_API_KEY, settings.OPENAI_MODEL)
