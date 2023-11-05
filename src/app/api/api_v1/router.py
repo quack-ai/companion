a8840fa77ef777e27b75609c49e38e985b6de139
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2023-11-06 00:18:19 +0100

    fix: Fixes the route input schemas for repos & guidelines (#25)
    * fix: Fixes schemas
    
    * fix: Fix router doc
    
    * refactor: Refactors args

--- src/app/api/api_v1/router.py
+++ src/app/api/api_v1/router.py
@@ -9,7 +9,7 @@ from app.api.api_v1.endpoints import compute, guidelines, login, repos, users
 
 api_router = APIRouter()
 api_router.include_router(login.router, prefix="/login", tags=["login"])
-api_router.include_router(users.router, prefix="/users", tags=["access"])
+api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(repos.router, prefix="/repos", tags=["repos"])
 api_router.include_router(guidelines.router, prefix="/guidelines", tags=["guidelines"])
 api_router.include_router(compute.router, prefix="/compute", tags=["compute"])
