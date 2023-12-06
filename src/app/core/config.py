b5d4f3c5f17ae14ca54f474e098dda4ef8bac16d
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2023-12-06 19:12:30 +0100

    refactor: Cleans up env vars

--- src/app/core/config.py
+++ src/app/core/config.py
@@ -5,6 +5,7 @@
 
 import os
 import secrets
+import socket
 from typing import Union
 
 from pydantic import BaseSettings, validator
@@ -21,22 +22,14 @@ class Settings(BaseSettings):
     VERSION: str = "0.1.0.dev0"
     API_V1_STR: str = "/api/v1"
     CORS_ORIGIN: str = "*"
+    # Authentication
+    SUPERADMIN_GH_PAT: str = os.environ["SUPERADMIN_GH_PAT"]
+    SUPERADMIN_PWD: str = os.environ["SUPERADMIN_PWD"]
     GH_OAUTH_ID: str = os.environ["GH_OAUTH_ID"]
     GH_OAUTH_SECRET: str = os.environ["GH_OAUTH_SECRET"]
     GH_TOKEN: Union[str, None] = os.environ.get("GH_TOKEN")
-    # Security
-    SECRET_KEY: str = os.environ.get("SECRET_KEY", secrets.token_urlsafe(32))
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
-    ACCESS_TOKEN_UNLIMITED_MINUTES: int = 60 * 24 * 365
-    JWT_ENCODING_ALGORITHM: str = "HS256"
     # DB
     POSTGRES_URL: str = os.environ["POSTGRES_URL"]
-    SUPERUSER_LOGIN: str = os.environ["SUPERUSER_LOGIN"]
-    SUPERUSER_ID: int = int(os.environ["SUPERUSER_ID"])
-    SUPERUSER_PWD: str = os.environ["SUPERUSER_PWD"]
-    # Compute
-    OPENAI_API_KEY: str = os.environ["OPENAI_API_KEY"]
-    OPENAI_MODEL: OpenAIModel = OpenAIModel.GPT3_5
 
     @validator("POSTGRES_URL", pre=True)
     @classmethod
@@ -46,8 +39,18 @@ class Settings(BaseSettings):
             return v.replace("postgres://", "postgresql+asyncpg://", 1)
         return v
 
+    # Security
+    SECRET_KEY: str = os.environ.get("SECRET_KEY", secrets.token_urlsafe(32))
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+    ACCESS_TOKEN_UNLIMITED_MINUTES: int = 60 * 24 * 365
+    JWT_ENCODING_ALGORITHM: str = "HS256"
+    # Compute
+    OPENAI_API_KEY: str = os.environ["OPENAI_API_KEY"]
+    OPENAI_MODEL: OpenAIModel = OpenAIModel.GPT4_TURBO
+
+    # Error monitoring
     SENTRY_DSN: Union[str, None] = os.environ.get("SENTRY_DSN")
-    SERVER_NAME: str = os.environ["SERVER_NAME"]
+    SERVER_NAME: str = os.environ.get("SERVER_NAME", socket.gethostname())
 
     @validator("SENTRY_DSN", pre=True)
     @classmethod
@@ -56,6 +59,7 @@ class Settings(BaseSettings):
             return None
         return v
 
+    # Product analytics
     POSTHOG_KEY: Union[str, None] = os.environ.get("POSTHOG_KEY")
 
     @validator("POSTHOG_KEY", pre=True)
@@ -65,6 +69,7 @@ class Settings(BaseSettings):
             return None
         return v
 
+    # Event notifications
     SLACK_API_TOKEN: Union[str, None] = os.environ.get("SLACK_API_TOKEN")
     SLACK_CHANNEL: str = os.environ.get("SLACK_CHANNEL", "#general")
 
