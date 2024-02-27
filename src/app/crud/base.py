2d19536ece5ca0b9a4a4189f7dc250e13a4ed782
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2024-02-27 13:29:26 +0100

    style(mypy): fix typing issues

--- src/app/crud/base.py
+++ src/app/crud/base.py
@@ -62,7 +62,7 @@ class BaseCRUD(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         statement = select(self.model)
         if isinstance(filter_pair, tuple):
             statement = statement.where(getattr(self.model, filter_pair[0]) == filter_pair[1])
-        return await self.session.exec(statement=statement)
+        return await self.session.exec(statement=statement)  # type: ignore[return-value]
 
     async def update(self, entry_id: int, payload: UpdateSchemaType) -> ModelType:
         access = cast(ModelType, await self.get(entry_id, strict=True))
@@ -81,5 +81,5 @@ class BaseCRUD(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         await self.get(entry_id, strict=True)
         statement = delete(self.model).where(self.model.id == entry_id)  # type: ignore[attr-defined]
 
-        await self.session.exec(statement=statement)
+        await self.session.exec(statement=statement)  # type: ignore[call-overload]
         await self.session.commit()
