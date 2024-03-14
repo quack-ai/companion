f79287d7ecb6d963ed08653facc1792a875f7707
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2024-03-14 18:18:00 +0100

    feat(demo): made auth UI optional

--- demo/main.py
+++ demo/main.py
@@ -7,34 +7,38 @@ import argparse
 import json
 import os
 from pathlib import Path
-from typing import List
+from typing import Dict, List
 
 import gradio as gr
 import requests
 from dotenv import load_dotenv
 
-load_dotenv()
-
-API_URL: str = os.getenv("API_URL", "http://localhost:8050/api/v1")
-LOGIN_ENDPOINT = f"{API_URL}/login/creds"
-CHAT_ENDPOINT = f"{API_URL}/code/chat"
-API_LOGIN: str = os.environ["SUPERADMIN_LOGIN"]
-API_PWD: str = os.environ["SUPERADMIN_PWD"]
-
 
 class SessionManager:
     def __init__(self) -> None:
         self._token = ""
+        self._url = ""
 
-    def set(self, token: str) -> None:
+    def set_token(self, token: str) -> None:
         self._token = token
 
+    def set_url(self, url: str) -> None:
+        self._url = url
+
     @property
-    def auth(self) -> str:
+    def auth(self) -> Dict[str, str]:
         return {
             "Authorization": f"Bearer {self._token}",
             "Content-Type": "application/json",
         }
+
+    @property
+    def login_endpoint(self) -> str:
+        return f"{self._url}/login/creds"
+
+    @property
+    def chat_endpoint(self) -> str:
+        return f"{self._url}/code/chat"
 
 
 session_manager = SessionManager()
@@ -53,7 +57,7 @@ def get_token(endpoint: str, login: str, pwd: str) -> str:
 
 def auth_gradio(username: str, password: str) -> bool:
     try:
-        session_manager.set(get_token(LOGIN_ENDPOINT, username, password))
+        session_manager.set_token(get_token(session_manager.login_endpoint, username, password))
         return True
     except ValueError:
         return False
@@ -68,7 +72,7 @@ def chat_response(message: str, history: List[List[str]]) -> str:
         for idx, msg in enumerate(hist)
     ]
     with session.post(
-        CHAT_ENDPOINT,
+        session_manager.chat_endpoint,
         json={"messages": [*_history, {"role": "user", "content": message}]},
         headers=session_manager.auth,
         stream=True,
@@ -80,6 +84,7 @@ def chat_response(message: str, history: List[List[str]]) -> str:
 
 
 def main(args: argparse.Namespace) -> None:
+    session_manager.set_url(args.api)
     # Run the interface
     interface = gr.ChatInterface(
         chat_response,
@@ -115,21 +120,33 @@ def main(args: argparse.Namespace) -> None:
         submit_btn=gr.Button("", variant="primary", size="sm", icon="./demo/assets/paper-plane.png"),
         stop_btn=gr.Button("", variant="stop", size="sm", icon="./demo/assets/stop-button.png"),
     )
+    if not args.auth:
+        session_manager.set_token(
+            get_token(session_manager.login_endpoint, os.environ["SUPERADMIN_LOGIN"], os.environ["SUPERADMIN_PWD"])
+        )
     interface.launch(
         server_port=args.port,
         show_error=True,
         favicon_path=Path(__file__).resolve().parent.joinpath("favicon.ico"),
-        auth=auth_gradio,
+        auth=auth_gradio if args.auth else None,
         show_api=False,
     )
 
 
 if __name__ == "__main__":
+    load_dotenv()
     parser = argparse.ArgumentParser(
         description="Quack API demo",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     parser.add_argument("--port", type=int, default=8001, help="Port on which the webserver will be run")
+    parser.add_argument(
+        "--api",
+        type=str,
+        default=os.getenv("API_URL", "http://localhost:8050/api/v1"),
+        help="URL of your Quack API instance",
+    )
+    parser.add_argument("--auth", action="store_true", help="Use the creds from Auth UI instead of env variables")
     args = parser.parse_args()
 
     main(args)
